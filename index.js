40d18ed71e4d20e7bb47ddccb1f811d639600d5f
NelsonAraujo93 <nelsonaraujoparedes93@hotmail.com>
2023-01-09 13:02:12 -0500

    Add remove function and PrintBooks from list

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 'use script'
-
 //declare variables
 const inputTitle = document.querySelector('.booktitle')
 const inputAuthor = document.querySelector('.bookauthor')
 const addBtn = document.querySelector('.add')
-const showBook = document.querySelector('.showbooks')
+const showBook = document.getElementById('books-list')
 const removeBtn = document.querySelector('.remove')
 
 // create an empty collection to store our books
@@ -15,27 +14,79 @@ function Book(title, author) {
   this.author = author;
 }
 
+let position = 0;
 // create a function to add a new book
 addBtn.addEventListener('click', (e) => {
   e.preventDefault();
-const theTitle = inputTitle.value
-const theAuthor = inputAuthor.value
-const newBook = new Book(theTitle, theAuthor)
-bookList.push(newBook)
-const html = ` 
-  <div class="addbook">
-    <p class="title">${theTitle}</p>
-    <p class="author">${theAuthor}</p>
-    <button class="remove">Remove</button>
-    <div class="underline"></div>
-  </div>
-`
-showBook.innerHTML += html;
+  const theTitle = inputTitle.value;
+  const theAuthor = inputAuthor.value;
+  const newBook = new Book(theTitle, theAuthor);
+  bookList.push(newBook);
 
-function clearInput() {
-  document.getElementById("form")[0].reset();
-}
+  const addBook = document.createElement('div');
+  addBook.className = 'addbook';
+  addBook.innerHTML = `<p class="title">${theTitle}</p>
+    <p class="author">${theAuthor}</p>
+  `
+  const btnRmv = document.createElement('button');
+  btnRmv.className = 'remove';
+  btnRmv.id = 'rmv-btn';
+  btnRmv.innerHTML = 'remove';
+  addBook.append(btnRmv);
+
+  const underline = document.createElement('div');
+  underline.className='underline';
+  addBook.append(underline);
+  showBook.append(addBook);
+
+
+  btnRmv.addEventListener('click', () => {
+    const newArray = bookList.filter(books => {
+      return books.title !== newBook.title ||
+      books.author !== newBook.author
+    });
+
+    bookList = newArray;
+    printBooks(bookList);
+  })
+
+  function clearInput() {
+    document.getElementById("form")[0].reset();
+  }
 })
+
+const printBooks = (bookList) => {
+  showBook.innerHTML = '';
+  bookList.map((item) => {
+    const addBook = document.createElement('div');
+    addBook.className = 'addbook';
+    addBook.innerHTML = `<p class="title">${item.title}</p>
+      <p class="author">${item.author}</p>
+    `
+    const btnRmv = document.createElement('button');
+    btnRmv.className = 'remove';
+    btnRmv.id = 'rmv-btn';
+    btnRmv.innerHTML = 'remove';
+    addBook.append(btnRmv);
+  
+    const underline = document.createElement('div');
+    underline.className='underline';
+    addBook.append(underline);
+    showBook.append(addBook);
+  
+  
+    btnRmv.addEventListener('click', () => {
+      const newArray = bookList.filter(books => {
+        return books.title !== item.title ||
+        books.author !== item.author
+      });
+  
+      bookList = newArray;
+      printBooks(bookList);
+      debugger;
+    })
+  })
+}
 
 //create a function to remove book
 
